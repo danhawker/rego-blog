7757cd4f6a5d6be6078c5170a2958046b9a0f5a2
garethahealy <garethahealy@gmail.com>
2020-07-29 15:21:40 +0100

    Added draft 2

--- test/conftest-tests.sh
+++ test/conftest-tests.sh
@@ -19,3 +19,13 @@ setup_file() {
   [ "${lines[1]}" = "not ok 1 - ${tmp}/list.yml - Deployment/imageuseslatesttag: container 'bar' is using the latest tag for its image (quay.io/redhat-cop/openshift-applier:latest), which is an anti-pattern." ]
   [ "${lines[2]}" = "" ]
 }
+
+@test "container-image-latest - show the conftest command" {
+  tmp=$(split_files "policy/container-image-latest/test_data/unit")
+
+  cmd="conftest test ${tmp} --output tap"
+  run ${cmd}
+
+  print_info "${status}" "${output}" "${cmd}" "${tmp}"
+  [ "$status" -eq 0 ]
+}
